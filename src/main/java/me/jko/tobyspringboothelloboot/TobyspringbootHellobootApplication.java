86ee869cfdf24e6d9cdcf334ef802c0030ebd766
junhee-ko <junheee.ko@gmail.com>
2023-03-23 23:47:27 +0900

    initial commit

---
+++ src/main/java/me/jko/tobyspringboothelloboot/TobyspringbootHellobootApplication.java
@@ -0,0 +1,13 @@
+package me.jko.tobyspringboothelloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyspringbootHellobootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TobyspringbootHellobootApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/jko/tobyspringboothelloboot/TobyspringbootHellobootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jko.tobyspringboothelloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyspringbootHellobootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
