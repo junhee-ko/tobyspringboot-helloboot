5acc90886e62aff8a13fbc16914d80828fe13af4
junhee-ko <junheee.ko@gmail.com>
2023-03-23 23:55:53 +0900

    Add Servlet Container and Servlet

--- src/main/java/me/jko/tobyspringboothelloboot/Application.java
+++ src/main/java/me/jko/tobyspringboothelloboot/Application.java
@@ -1,13 +1,39 @@
 package me.jko.tobyspringboothelloboot;
 
-import org.springframework.boot.SpringApplication;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+    WebServer webServer = serverFactory.getWebServer(servletContext -> {
+      servletContext.addServlet("hello", new HttpServlet() {
+        @Override
+        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          String name = req.getParameter("name");
+
+          resp.setStatus(HttpStatus.OK.value());
+          resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+          resp.getWriter().println("Hello Servlet " + name);
+        }
+      }).addMapping("/hello");
+    });
+
+    webServer.start();
   }
 
 }
