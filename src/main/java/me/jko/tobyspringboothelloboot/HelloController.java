04d4555e1ca38a826b756378a764340f75546398
junhee-ko <junheee.ko@gmail.com>
2023-03-26 16:09:31 +0900

    Add mapping annotation in HelloController

--- src/main/java/me/jko/tobyspringboothelloboot/HelloController.java
+++ src/main/java/me/jko/tobyspringboothelloboot/HelloController.java
@@ -1,7 +1,12 @@
 package me.jko.tobyspringboothelloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
   private final HelloService helloService;
 
@@ -9,9 +14,14 @@ public class HelloController {
     this.helloService = helloService;
   }
 
+  /*
+   * - "hello spring" 을 리턴하면, DispatcherServlet 이 "hello spring" 이름의 뷰가 있는지 확인. 없으면 404
+   * - "hello spring" 을 리턴하면, DispatcherServlet 이 웹 응답의 바디로 넣어서 text-plain 으로 전달하게 하려면 "@ResponseBody" 를 추가해야함
+   * - " @ResponseBody" 를 추가 안하려면, @RestController 를 붙이면 됨 -> DispatcherServlet 은 그럼 모든 메서드에 "@ResponseBody" 가 붙여있다고 가정함
+   * */
+  @GetMapping
+  @ResponseBody
   public String hello(String name) {
-    HelloService helloService = new SimpleHelloService();
-
     return helloService.sayHello(Objects.requireNonNull(name));
   }
 }
