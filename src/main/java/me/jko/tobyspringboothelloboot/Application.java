5eebfdd059b022bc325bebe0fc884a48bcb375e1
junhee-ko <junheee.ko@gmail.com>
2023-03-26 17:41:06 +0900

    Extract MySpringApplication

--- src/main/java/me/jko/tobyspringboothelloboot/Application.java
+++ src/main/java/me/jko/tobyspringboothelloboot/Application.java
@@ -1,12 +1,10 @@
 package me.jko.tobyspringboothelloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -14,32 +12,9 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class Application {
 
   public static void main(String[] args) {
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
-
-        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-        /*
-         * Spring Container 가 DispatcherServlet 은 ApplicationContext 가 필요하다고 판단해서 주입을 해줌
-         * by ApplicationContextAware setApplicationContext
-         * */
-        // dispatcherServlet.setApplicationContext(this);
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
-        });
-
-        webServer.start();
-      }
-    };
-
-    applicationContext.register(Application.class);
-    applicationContext.refresh();
+    MySpringApplication.run(Application.class, args);
   }
+
 
   @Bean
   public ServletWebServerFactory servletWebServerFactory() {
---
+++ src/main/java/me/jko/tobyspringboothelloboot/MySpringApplication.java
@@ -0,0 +1,37 @@
+package me.jko.tobyspringboothelloboot;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+
+  public static void run(Class<?> applicationClass, String[] args) {
+    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+
+      @Override
+      protected void onRefresh() {
+        super.onRefresh();
+
+        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+
+        /*
+         * Spring Container 가 DispatcherServlet 은 ApplicationContext 가 필요하다고 판단해서 주입을 해줌
+         * by ApplicationContextAware setApplicationContext
+         * */
+        // dispatcherServlet.setApplicationContext(this);
+
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+          servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
+        });
+
+        webServer.start();
+      }
+    };
+
+    applicationContext.register(applicationClass);
+    applicationContext.refresh();
+  }
+}
