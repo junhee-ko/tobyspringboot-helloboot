0d9cd30f87fd527af8c933042ff1c4115580d7cb
junhee-ko <junheee.ko@gmail.com>
2023-03-26 14:21:27 +0900

    Update front controller to get bean from application context

--- src/main/java/me/jko/tobyspringboothelloboot/Application.java
+++ src/main/java/me/jko/tobyspringboothelloboot/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -19,11 +20,12 @@ import java.io.IOException;
 public class Application {
 
   public static void main(String[] args) {
+    GenericApplicationContext applicationContext = new GenericApplicationContext();
+    applicationContext.registerBean(HelloController.class);
+    applicationContext.refresh();
+
     ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      HelloController helloController = new HelloController();
-
       servletContext.addServlet("hello", new HttpServlet() {
         @Override
         protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,6 +33,7 @@ public class Application {
           if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
             String name = req.getParameter("name");
 
+            HelloController helloController = applicationContext.getBean(HelloController.class);
             String ret = helloController.hello(name);
 
             resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
