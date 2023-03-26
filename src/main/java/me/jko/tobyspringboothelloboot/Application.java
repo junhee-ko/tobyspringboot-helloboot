88a082e772e1345e578f90d6d5810b958e2b60b6
junhee-ko <junheee.ko@gmail.com>
2023-03-26 16:19:22 +0900

    Integrate spring container init and servlet container init

--- src/main/java/me/jko/tobyspringboothelloboot/Application.java
+++ src/main/java/me/jko/tobyspringboothelloboot/Application.java
@@ -11,17 +11,27 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class Application {
 
   public static void main(String[] args) {
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+
+      @Override
+      protected void onRefresh() {
+        super.onRefresh();
+
+        // 서블릿 컨테이너를 만들고 서블릿 초기화 작업을 스프링 컨테이너가 초기화 되는 과정 중에.
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+          servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)).addMapping("/*");
+        });
+
+        webServer.start();
+      }
+    };
+
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(SimpleHelloService.class);
     applicationContext.refresh();
 
-    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext)).addMapping("/*");
-    });
 
-    webServer.start();
   }
 
 }
