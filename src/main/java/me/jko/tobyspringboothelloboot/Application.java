38732298ecbbe2984a3524c98edababfc740af36
junhee-ko <junheee.ko@gmail.com>
2023-04-15 21:11:16 +0900

    Add MySpringBootAnnotation and Config class

--- src/main/java/me/jko/tobyspringboothelloboot/Application.java
+++ src/main/java/me/jko/tobyspringboothelloboot/Application.java
@@ -1,29 +1,11 @@
 package me.jko.tobyspringboothelloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootAnnotation
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-
-  @Bean
-  public ServletWebServerFactory servletWebServerFactory() {
-    return new TomcatServletWebServerFactory();
-  }
-
-  @Bean
-  public DispatcherServlet dispatcherServlet() {
-    return new DispatcherServlet();
   }
 }
