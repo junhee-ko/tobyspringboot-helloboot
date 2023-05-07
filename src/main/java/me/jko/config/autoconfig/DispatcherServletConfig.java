77743589ffcedd1af0643401a5f4a4dd78f52f20
junhee-ko <junheee.ko@gmail.com>
2023-05-07 14:13:28 +0900

    Update proxyBeanMethods to false in MyAutoConfiguration

--- src/main/java/me/jko/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/me/jko/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package me.jko.config.autoconfig;
 
+import me.jko.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
