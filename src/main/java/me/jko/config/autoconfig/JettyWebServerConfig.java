78a07321280e7671012f4a87d7e07407dd15dd60
junhee-ko <junheee.ko@gmail.com>
2023-05-07 14:54:30 +0900

    Add JettyWebServerConfig

--- src/main/java/me/jko/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/me/jko/config/autoconfig/JettyWebServerConfig.java
@@ -1,15 +1,15 @@
 package me.jko.config.autoconfig;
 
 import me.jko.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-    @Bean("tomcatWebServerFactory")
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
